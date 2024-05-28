afea8a6d32700906c9c67d1af38e7450a54bb5ec
Muhammad Sipa Fauzi <92158775+sipafauzi@users.noreply.github.com>
2024-05-28 18:39:24 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/Main.java
@@ -0,0 +1,22 @@
+package com.juaracoding;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.printf("Aku Juara");
+
+        System.out.println("Hello World");
+        System.out.println("Hello Kode");
+        // blok program if
+        if( true ){
+            System.out.println("True");
+        }
+        // blok program for
+        for ( int i = 0; i<10; i++){
+            System.out.println("Perulangan ke"+i);
+        }
+    }
+
+
+}
+
